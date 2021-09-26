404621bd6cb84ada782b8fb509d95f455108711e
Ian Huang <21ianh1@students.tas.tw>
2021-09-26 17:59:19 -0500

    fix: move backend to typescript

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   paths: (paths, _) => {
-    paths.appIndexJs = path.resolve(__dirname, 'client/index.tsx');
-    paths.appSrc = path.resolve(__dirname, 'client');
-    paths.appBuild = path.resolve(__dirname, 'dist');
+    paths.appIndexJs = path.resolve(__dirname, "client/index.tsx");
+    paths.appSrc = path.resolve(__dirname, "client");
+    paths.appBuild = path.resolve(__dirname, "dist");
     paths.appTypeDeclarations = path.resolve(
       __dirname,
-      'client/react-app-env.d.ts',
+      "client/react-app-env.d.ts"
     );
 
     return paths;
---
+++ server/index.ts
@@ -0,0 +1,5 @@
+function testtest(num: number) {
+  return num;
+}
+
+console.log(testtest(3));
